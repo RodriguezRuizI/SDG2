131ebc20799eb2be5b87f8ed8e079e598bfb8dbf
RodriguezRuizI <33327123+RodriguezRuizI@users.noreply.github.com>
2018-02-23 16:30:29 +0100

    inicio versión 2

---
+++ V2/estados.c
@@ -0,0 +1,8 @@
+/*
+ * estados.c
+ *
+ *  Created on: 23 de feb. de 2018
+ *      Author: alumno
+ */
+
+
---
+++ V2/estados.h
@@ -0,0 +1,27 @@
+/*
+ * estados.h
+ *
+ *  Created on: 23 de feb. de 2018
+ *      Author: alumno
+ */
+
+#ifndef ESTADOS_H_
+#define ESTADOS_H_
+
+
+
+#endif /* ESTADOS_H_ */
+
+#include <stdlib.h> // para poder usar NULL
+#include <stdio.h> // para poder usar printf
+#include <time.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <wiringPi.h>
+
+#include "kbhit.h" // para poder detectar teclas pulsadas sin bloqueo y leer las teclas pulsadas
+
+#include "fsm.h" // para poder crear y ejecutar la máquina de estados
