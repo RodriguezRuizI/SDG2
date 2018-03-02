4647f10692ebba4166d803f1692a4e78a501e194
RodriguezRuizI <33327123+RodriguezRuizI@users.noreply.github.com>
2018-03-02 12:15:07 +0100

    faltan estados y el main

--- V2/estados.h
+++ V2/estados.h
@@ -21,7 +21,22 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <wiringPi.h>
-
+#include <softTone.h>
 #include "kbhit.h" // para poder detectar teclas pulsadas sin bloqueo y leer las teclas pulsadas
-
+#include "piMusicBox_2.h"
 #include "fsm.h" // para poder crear y ejecutar la máquina de estados
+
+
+//Funciones de entrada
+int CompruebaPlayerStart(fsm_t* this);
+int CompruebaPlayerStop(fsm_t* this);
+int CompruebaNuevaNota(fsm_t* this);
+int CompruebaNotaTimeout(fsm_t* this);
+int CompruebaFinalMelodia(fsm_t* this);
+
+//Funciones de salida
+void InicializaPlayer(fsm_t* this);
+void StopPlayer(fsm_t* this);
+void ComienzaNuevaNota(fsm_t* this);
+void ActulizaPlayer(fsm_t* this);
+void FinalMelodia(fsm_t* this);
